1b3ef757a3465d1c5081fc13a823c9c189a0723e
Ricardo <ricardo.vejarano.garcia@gmail.com>
2020-03-10 21:34:16 -0500

    Basic structure for first page

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,12 @@
 <template>
-  <div class="container">
-    <h1>Test bootstrap</h1>
+  <div id="main-content">
+    <div class="container">
+      <div class="row align-middle align-items-center card-login">
+        <div class="col-6 mx-auto">
+          <div class="jumbotron">Welcome to the loan simulated system</div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -12,12 +18,21 @@ export default {
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+#main-content {
+  overflow-x: hidden;
+  overflow-y: hidden;
+  background-color: rgba(226, 226, 226, 1);
+}
+#main-content .card-login {
+  height: 100vh;
+}
+
+.jumbotron {
+  box-shadow: 0 1px 3px rgba(0, 0, 0, 0.12), 0 1px 2px rgba(0, 0, 0, 0.24);
+  transition: all 0.3s cubic-bezier(0.25, 0.8, 0.25, 1);
+}
+
+.jumbotron:hover {
+  box-shadow: 0 14px 28px rgba(0, 0, 0, 0.25), 0 10px 10px rgba(0, 0, 0, 0.22);
 }
 </style>
