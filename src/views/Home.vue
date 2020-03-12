68e56eb7619849a7be139a04aaa8ae1f3a4242bf
Ricardo <ricardo.vejarano.garcia@gmail.com>
2020-03-12 10:00:16 -0500

    Improvements in home

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,10 +1,10 @@
 <template>
   <div class="container">
     <div class="row align-middle align-items-center card-login">
-      <div class="col-6 mx-auto">
+      <div class="col-12 col-md-6 mx-auto">
         <div class="jumbotron text-center">
           <div class="row mb-3">
-            <div class="col-4 offset-4 title-txt">
+            <div class="col-12 col-lg-4 offset-lg-4 title-txt">
               <span>Welcome to the loan simulation</span>
             </div>
           </div>
@@ -31,16 +31,12 @@ export default {
 </script>
 
 <style>
-#main-content {
-  overflow-x: hidden;
-  overflow-y: hidden;
-  background-color: rgba(226, 226, 226, 1);
-}
 .card-login {
   height: 100vh;
 }
 
 .jumbotron {
+  min-width: 17rem;
   box-shadow: 0 1px 3px rgba(0, 0, 0, 0.12), 0 1px 2px rgba(0, 0, 0, 0.24);
   border-radius: 1rem;
   transition: all 0.3s cubic-bezier(0.25, 0.8, 0.25, 1);
